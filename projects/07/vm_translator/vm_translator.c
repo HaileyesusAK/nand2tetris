9ff0e3980207df0d2ab81dc97478a48481e265e5
haileyesus <hailejesus@gmail.com>
2020-04-05 14:14:13 +0200

    concluded first part of vm translator

--- projects/07/vm_translator/vm_translator.c
+++ projects/07/vm_translator/vm_translator.c
@@ -12,8 +12,7 @@
 
 #define CMD_LEN				9
 #define SEGMENT_LEN			9
-#define N_VM_INST			29
-#define VM_INST_LEN			64
+#define VM_LINE_LEN			256
 
 typedef enum {
 	POINTER = 3, //RAM location 3, coincides with THIS
@@ -32,20 +31,26 @@ typedef enum {
 	ALU_INST
 }InstType;
 
+typedef struct  {
+	size_t n_cmds;
+	char **cmds;
+	struct hsearch_data* generator_mapping;
+	char *static_prefix;
+}VmTranslator;
+
 typedef size_t Generator(FILE *, ...);
 
 
-static void *RAM = NULL;
 static uint16_t PC;				//Program counter, used when generating instruction for logical operations
 static char* static_prefix;
 
-static struct hsearch_data generator_mapping;
 
 static size_t parse_word(const char* src, char* dst, size_t n_dst);
 static int parse_vm_inst(const char* vm_inst, char *cmd, InstType* inst_type,
 						 char *segment, uint16_t* idx);
 
-static int translate_vm_inst(const char* vm_inst, FILE* asm_inst, size_t* inst_len);
+static int translate_vm_inst(const VmTranslator* vm_translator,
+							 const char* vm_inst, FILE* asm_inst, size_t* inst_len);
 
 static size_t gen_add_asm(FILE *asm_file);
 static size_t gen_and_asm(FILE *asm_file);
@@ -78,30 +83,44 @@ static size_t gen_pop_temp_asm(FILE *asm_file, uint16_t idx);
 static size_t gen_push_const_asm(FILE *asm_file, uint16_t idx);
 static size_t gen_pop_const_asm(FILE *asm_file, uint16_t idx);
 
-void *init_translator(char *prefix)
+void *init_vm_translator(char *prefix)
 {
+
 	if(!prefix)
-		return NULL;
-
-	static_prefix = strdup(prefix);
-	if(!static_prefix)
-		return NULL;
-
-	if(!hcreate_r(N_VM_INST, &generator_mapping))
-		return NULL;
-
-	RAM = calloc(1, RAM_SIZE);
-	if(!RAM)
 	{
-		hdestroy_r(&generator_mapping);
-		free(static_prefix);
+		fprintf(stderr, "NULL input pointer at %s, line %d\n", __FILE__, __LINE__);
 		return NULL;
 	}
 
-	struct {
+	VmTranslator* vm_translator = calloc(1, sizeof(VmTranslator));
+	if(!vm_translator)
+	{
+		fprintf(stderr, "calloc failed at %s, line %d\n", __FILE__, __LINE__);
+		return NULL;
+	}
+	
+	vm_translator->generator_mapping = calloc(1, sizeof(struct hsearch_data));
+	if(!vm_translator->generator_mapping)
+	{
+		fprintf(stderr, "calloc failed at %s, line %d\n", __FILE__, __LINE__);
+		destroy_vm_translator(vm_translator);
+		return NULL;
+	}
+
+	vm_translator->static_prefix = strdup(prefix);
+	static_prefix = vm_translator->static_prefix;
+
+	if(!vm_translator->static_prefix)
+	{
+		fprintf(stderr, "strdup failed at %s, line %d\n", __FILE__, __LINE__);
+		destroy_vm_translator(vm_translator);
+		return NULL;
+	}
+	
+	struct CmdGen{
 		char *cmd;
 		void *generator;
-	} cmd_generators[N_VM_INST] = { 
+	} cmd_generators[] = { 
 		{"add",  gen_add_asm}, {"sub", gen_sub_asm}, {"neg", gen_neg_asm},
 		{"eq", gen_eq_asm}, {"gt", gen_gt_asm}, {"lt", gen_lt_asm},
 		{"and", gen_and_asm}, {"or", gen_or_asm}, {"not", gen_not_asm},
@@ -112,39 +131,86 @@ void *init_translator(char *prefix)
 		{"push_pointer", gen_push_ptr_asm}, {"pop_pointer", gen_pop_ptr_asm},
 		{"push_temp", gen_push_temp_asm}, {"pop_temp", gen_pop_temp_asm},
 		{"push_static", gen_push_static_asm}, {"pop_static", gen_pop_static_asm},
-		{"push_constant", gen_push_const_asm}, {"pop_constant", gen_pop_const_asm},
+		{"push_constant", gen_push_const_asm}, {"pop_constant", gen_pop_const_asm}
 	};
+
+	vm_translator->n_cmds = sizeof(cmd_generators) / sizeof(struct CmdGen);	
+	vm_translator->cmds = calloc(1, vm_translator->n_cmds * sizeof(char*));
+	if(!vm_translator->cmds)
+	{
+		fprintf(stderr, "calloc failed at %s, line %d\n", __FILE__, __LINE__);
+		destroy_vm_translator(vm_translator);
+		return NULL;
+	}
+	
+	for(size_t i = 0; i < vm_translator->n_cmds; ++i)
+	{
+		vm_translator->cmds[i] = strdup(cmd_generators[i].cmd);
+		if(!vm_translator->cmds[i])
+		{
+			fprintf(stderr, "strdup failed at %s, line %d\n", __FILE__, __LINE__);
+			destroy_vm_translator(vm_translator);
+			return NULL;
+		}
+	}
+	
+	if(!hcreate_r(vm_translator->n_cmds, vm_translator->generator_mapping))
+	{
+		fprintf(stderr, "hcreate_r failed at %s, line %d\n", __FILE__, __LINE__);
+		destroy_vm_translator(vm_translator);
+		return NULL;
+	}
 
 	size_t i = 0; 
 	ENTRY item, *entry;
-	for(i = 0; i < N_VM_INST; ++i)
+	for(i = 0; i < vm_translator->n_cmds; ++i)
 	{
-		item.key = cmd_generators[i].cmd;
-		item.data = (void*)cmd_generators[i].generator;
-		hsearch_r(item, ENTER, &entry, &generator_mapping);
+		item.key = vm_translator->cmds[i];
+		item.data = cmd_generators[i].generator;
+		if(!hsearch_r(item, ENTER, &entry, vm_translator->generator_mapping))
+		{
+			fprintf(stderr, "hsearch_r failed at %s, line %d\n", __FILE__, __LINE__);
+			destroy_vm_translator(vm_translator);
+			return NULL;
+		}
 	}
 	
-	return RAM;
+	return vm_translator;
 }
 
-void destroy_vm_translator(void* vm_translator)
+void destroy_vm_translator(void* handle)
 {
+	VmTranslator *vm_translator = (VmTranslator *)handle;
 	if(vm_translator)
 	{
-		free(RAM);
-		free(static_prefix);
-		hdestroy_r(&generator_mapping);
+		if(vm_translator->static_prefix)
+			free(vm_translator->static_prefix);
+		
+		if(vm_translator->cmds)
+		{
+			for(size_t i = 0; i < vm_translator->n_cmds; ++i)
+				if(vm_translator->cmds[i])
+					free(vm_translator->cmds[i]);
+			free(vm_translator->cmds);
+		}
+		
+		if(vm_translator->generator_mapping)
+		{
+			hdestroy_r(vm_translator->generator_mapping);
+			free(vm_translator->generator_mapping);
+		}
+		
+		free(vm_translator);
 	}
 }
 
-int translate_vm_inst(const char* vm_inst, FILE* asm_inst, size_t* inst_len)
+int translate_vm_inst(const VmTranslator* vm_translator, const char* vm_inst, FILE* asm_inst, size_t* inst_len)
 {
 	char cmd[CMD_LEN];
 	char segment[SEGMENT_LEN];
 	uint16_t idx;
 	InstType inst_type;
 	ENTRY item, *entry;
-
 	size_t len = 0;
 
 	parse_vm_inst(vm_inst, cmd, &inst_type, segment, &idx);
@@ -156,9 +222,13 @@ int translate_vm_inst(const char* vm_inst, FILE* asm_inst, size_t* inst_len)
 			char key[CMD_LEN + SEGMENT_LEN + 1];
 			snprintf(key, CMD_LEN + SEGMENT_LEN + 1, "%s_%s", cmd, segment);
 			item.key = key;
-			if(!hsearch_r(item, FIND, &entry, &generator_mapping))
-				return -2;
-			
+			if(!hsearch_r(item, FIND, &entry, vm_translator->generator_mapping))
+			{
+				fprintf(stderr, "Cannot translate VM command '%s %s' at %s, line %d\n",
+								cmd, segment, __FILE__, __LINE__);
+				return -1;
+			}	
+
 			Generator *generator = (Generator*)entry->data;
 			len = generator(asm_inst, idx);
 		}
@@ -167,9 +237,13 @@ int translate_vm_inst(const char* vm_inst, FILE* asm_inst, size_t* inst_len)
 		case ALU_INST:
 		{
 			item.key = cmd;
-			if(!hsearch_r(item, FIND, &entry, &generator_mapping))
-				return -2;
-			
+			if(!hsearch_r(item, FIND, &entry, vm_translator->generator_mapping))
+			{
+				fprintf(stderr, "Cannot translate VM command '%s' at %s, line %d\n",
+								cmd, __FILE__, __LINE__);
+				return -1;
+			}
+
 			Generator *generator = (Generator*)entry->data;
 			len = generator(asm_inst);
 		}
@@ -179,34 +253,25 @@ int translate_vm_inst(const char* vm_inst, FILE* asm_inst, size_t* inst_len)
 			break;
 	}
 
-	PC += len;
 	*inst_len = len;
 	
 	return 0;
 }
 
-int translate_vm_file(FILE* vm_file, FILE* asm_file)
+int translate_vm_file(const void* handle, FILE* vm_file, FILE* asm_file)
 {
-	if(!vm_file || !asm_file)
+	if(!handle || !vm_file || !asm_file)
+	{
+		fprintf(stderr, "NULL input pointer at %s, line %d\n", __FILE__, __LINE__);
 		return -1;
+	}
 	
 	size_t inst_len;
-	int rc;
-	PC = 0;
-	char vm_inst[VM_INST_LEN];
-	while(fgets(vm_inst, VM_INST_LEN, vm_file))
-	{
-		fprintf(asm_file, "//%s", vm_inst);
-		rc = translate_vm_inst(vm_inst, asm_file, &inst_len);
-		if(rc)
-			return -2;	//TODO: properly handle errors
+	char vm_inst[VM_LINE_LEN];
+	VmTranslator* vm_translator = (VmTranslator*) handle;
 
-		PC += inst_len;
-	}
-
-	//End of translation
-	fprintf(asm_file, "(END)\n"
-					  "\t0;JMP\n");
+	fprintf(asm_file, "\t@BEGIN\n"
+					  "\t0;JMP\n\n");
 
 	fprintf(asm_file, "(PUSH_TRUE)\n"
 					  "\t@SP\n"
@@ -215,7 +280,8 @@ int translate_vm_file(FILE* vm_file, FILE* asm_file)
 					  "\t@SP\n"
 					  "\tM=M+1\n"
 					  "\t@R13\n"
-					  "\t0;JMP\n");
+					  "\tA=M\n"
+					  "\t0;JMP\n\n");
 
 	fprintf(asm_file, "(PUSH_FALSE)\n"
 					  "\t@SP\n"
@@ -224,7 +290,38 @@ int translate_vm_file(FILE* vm_file, FILE* asm_file)
 					  "\t@SP\n"
 					  "\tM=M+1\n"
 					  "\t@R13\n"
-					  "\t0;JMP\n");
+					  "\tA=M\n"
+					  "\t0;JMP\n\n");
+
+	fprintf(asm_file, "(BEGIN)\n");
+	const size_t N_BEG_LINES = 18;
+	PC = N_BEG_LINES;	//The line number of the next assembly instruction
+	while(fgets(vm_inst, VM_LINE_LEN, vm_file))
+	{
+		//vm_inst[strlen(vm_inst)-1] = '\0';
+		int len = fprintf(asm_file, "//%s", vm_inst);
+		int rc = translate_vm_inst(vm_translator, vm_inst, asm_file, &inst_len);
+		if(rc)
+		{
+			fprintf(stderr, "Cannot translate VM instruction '%s' at %s, line %d\n",
+							vm_inst, __FILE__, __LINE__);
+			return -1;	
+		}
+
+		//Overwrite comment if no instruction is translated
+		if(!inst_len)
+			fseek(asm_file, -len, SEEK_CUR); 
+		else
+			fprintf(asm_file, "\n");
+				
+		PC += inst_len;
+	}
+
+	//End of translation
+	fprintf(asm_file, "(END)\n"
+					  "\t@END\n"
+					  "\t0;JMP\n\n");
+
 	return 0;
 }
 
@@ -236,7 +333,7 @@ size_t parse_word(const char* src, char* dst, size_t n_dst)
 	while(*c && isblank(*c))
 		c++;
 	
-	while(*c && !isblank(*c) && i < n_dst - 1)
+	while(*c && !isblank(*c) && !isspace(*c) && i < n_dst - 1)
 		dst[i++] = *c++;
 
 	dst[i] = '\0';
@@ -251,19 +348,19 @@ int parse_vm_inst(const char* vm_inst, char *cmd, InstType* inst_type,
 	size_t word_len;	
 	*inst_type = BLANK;
 
-	while(*c && isblank(*c))
+	while(*c && (isblank(*c) || isspace(*c)))
 		c++;	
 	
 	if(!*c || *c == '/') 
 		return 0;
 
 	*inst_type = ALU_INST;
+	
 	word_len = parse_word(c, cmd, CMD_LEN);
 	if(!strcmp(cmd, "push") || !strcmp(cmd, "pop"))
 	{
 		*inst_type = MEM_INST;
-		word_len = parse_word(c + word_len, segment, SEGMENT_LEN);
-		sscanf(c + word_len, "%hu", idx);
+		sscanf(c + word_len, "%s %hu", segment, idx);
 	}
 
 	return 0;
@@ -273,18 +370,20 @@ int parse_vm_inst(const char* vm_inst, char *cmd, InstType* inst_type,
 static size_t gen_binary_alu_op_asm(FILE *asm_file, char op)
 {
 
-	size_t len = fprintf(asm_file,
-						 "@SP\n"
-						 "AM=M-1\n"
-						 "D=M\n"
-						 "@SP\n"
-						 "A=M-1\n");
+	size_t n_lines = 6;
+	fprintf(asm_file,
+			"\t@SP\n"
+			"\tAM=M-1\n"
+			"\tD=M\n"
+			"\t@SP\n"
+			"\tA=M-1\n");
+	
 	if(op == '-')
-		len += fprintf(asm_file, "M=M-D\n");
+		fprintf(asm_file, "\tM=M-D\n");
 	else
-		len += fprintf(asm_file, "M=D%cM\n", op);
+		fprintf(asm_file, "\tM=D%cM\n", op);
 
-	return len;
+	return n_lines;
 }
 
 static size_t gen_add_asm(FILE *asm_file)
@@ -309,20 +408,33 @@ static size_t gen_and_asm(FILE *asm_file)
 
 static size_t gen_not_asm(FILE *asm_file)
 {
-	return fprintf(asm_file, "@SP\nA=M-1\nM=!M\n");
+	size_t n_lines = 3;
+	fprintf(asm_file, 
+			"\t@SP\n"
+			"\tA=M-1\n"
+			"\tM=!M\n");
+	
+	return n_lines;
 }
 
 static size_t gen_neg_asm(FILE *asm_file)
 {
-	return fprintf(asm_file, "@SP\nA=M-1\nM=-M\n");
+	size_t n_lines = 3;
+	fprintf(asm_file,
+		    "\t@SP\n"
+		    "\tA=M-1\n"
+		    "\tM=-M\n");
+	
+	return n_lines;
 }
 
 static size_t gen_binary_rel_asm(FILE *asm_file, char op)
 {
 
-	uint16_t return_addr = PC + 14;
-
+	size_t n_lines =  14;
+	uint16_t return_addr = PC + n_lines;
 	char *jmp_cmd;
+	
 	switch(op)
 	{
 		case '=': jmp_cmd = "JEQ"; break;
@@ -331,26 +443,26 @@ static size_t gen_binary_rel_asm(FILE *asm_file, char op)
 	}
 
 	//Save return address on R13
-	size_t len = fprintf(asm_file, "@%hu\n"
-								   "D=A\n"
-								   "@R13\n"
-								   "M=D\n",
-								   return_addr);
+	fprintf(asm_file, "\t@%hu\n"
+				      "\tD=A\n"
+				      "\t@R13\n"
+				      "\tM=D\n",
+				      return_addr);
 
 	//Compute x - y and put the result on D
-	len += fprintf(asm_file + len, "@SP\n"
-								   "AM=M-1\n"
-								   "D=M\n"
-								   "@SP\n"
-								   "AM=M-1\n"
-								   "D=M-D\n");
+	fprintf(asm_file, "\t@SP\n"
+					  "\tAM=M-1\n"
+					  "\tD=M\n"
+					  "\t@SP\n"
+					  "\tAM=M-1\n"
+					  "\tD=M-D\n");
 	
-	len += fprintf(asm_file + len, "@PUSH_TRUE\n"
-								   "D;%s\n"
-								   "@PUSH_FALSE\n"
-								   "0;JMP\n",
-								   jmp_cmd);
-	return len;
+	fprintf(asm_file, "\t@PUSH_TRUE\n"
+					  "\tD;%s\n"
+					  "\t@PUSH_FALSE\n"
+					  "\t0;JMP\n",
+					  jmp_cmd);
+	return n_lines;
 }
 
 static size_t gen_eq_asm(FILE *asm_file)
@@ -370,20 +482,28 @@ static size_t gen_gt_asm(FILE *asm_file)
 
 static size_t gen_push_asm(FILE *asm_file, SegmentBase seg_base, uint16_t idx)
 {
-	size_t len = 0;
+	size_t n_lines = 5;
 	char *push_D = "@SP\n"
-				   "A=M\n"
-				   "M=D\n"
-				   "@SP\n"
-				   "M=M+1";
+				   "\tA=M\n"
+				   "\tM=D\n"
+				   "\t@SP\n"
+				   "\tM=M+1";
 					
 	if(seg_base == CONSTANT)
 	{
-		len = fprintf(asm_file, "@%hu\nD=A\n%s\n", idx, push_D);
+		fprintf(asm_file,
+				"\t@%hu\n"
+				"\tD=A\n"
+				"\t%s\n", idx, push_D);
+		n_lines += 2;
 	}
 	else if(seg_base == STATIC)
 	{
-		len = fprintf(asm_file, "@%s.%hu\nD=M\n%s\n", static_prefix, idx, push_D);
+		fprintf(asm_file,
+			    "\t@%s.%hu\n"
+			    "\tD=M\n"
+			    "\t%s\n", static_prefix, idx, push_D);
+		n_lines += 2;
 	}
 	else
 	{
@@ -403,35 +523,48 @@ static size_t gen_push_asm(FILE *asm_file, SegmentBase seg_base, uint16_t idx)
 			case ARG:
 			case THIS:
 			case THAT:
-				len = fprintf(asm_file,
-							  "@%hu\n"
-							  "D=A\n"
-							  "@%s\n"
-							  "A=D+M\n"
-							  "D=M\n"
-							  "%s\n",
-							  idx, symbol, push_D);
+				n_lines += 5;
+				fprintf(asm_file,
+						"\t@%hu\n"
+						"\tD=A\n"
+						"\t@%s\n"
+						"\tA=D+M\n"
+						"\tD=M\n"
+						"\t%s\n",
+						idx, symbol, push_D);
 				break;
 			case POINTER:
 			case TEMP:
-				len = fprintf(asm_file, "@%hu\nD=M\n%s\n", seg_base, push_D);
+				n_lines += 2;
+				fprintf(asm_file,
+						"\t@%hu\n"
+						"\tD=M\n"
+						"\t%s\n", seg_base + idx, push_D);
 				break;
 			default:
 				break;
 		}
 	}
-	return len;
+	return n_lines;
 }
 
 static size_t gen_pop_asm(FILE *asm_file, SegmentBase seg_base, uint16_t idx)
 {
-	size_t len;
 	char *pop_D = "@SP\n"
-				  "AM=M-1\n"
-				  "D=M";
+				  "\tAM=M-1\n"
+				  "\tD=M";
+	
+	size_t n_lines = 3;
 
 	if(seg_base == STATIC)
-		len = fprintf(asm_file, "%s\n@%s.%hu\nM=D\n", pop_D, static_prefix, idx);
+	{
+		fprintf(asm_file,
+				"\t%s\n"
+				"\t@%s.%hu\n"
+				"\tM=D\n",
+				pop_D, static_prefix, idx);
+		n_lines += 2;
+	}
 	else
 	{
 	
@@ -451,31 +584,36 @@ static size_t gen_pop_asm(FILE *asm_file, SegmentBase seg_base, uint16_t idx)
 			case ARG:
 			case THIS:
 			case THAT:
-				len = fprintf(asm_file,
-							  "@%hu\n"
-							  "D=A\n"
-							  "@%s\n"
-							  "M=D+M\n"
-							  "%s\n"
-							  "@%s\n"
-							  "A=M\n"
-							  "M=D\n"
-							  "@%hu\n"
-							  "D=A\n"
-							  "@%s\n"
-							  "M=M-D\n",
-							  idx, symbol, pop_D, symbol, idx, symbol);
+				fprintf(asm_file,
+						"\t@%hu\n"
+						"\tD=A\n"
+						"\t@%s\n"
+						"\tM=D+M\n"
+						"\t%s\n"
+						"\t@%s\n"
+						"\tA=M\n"
+						"\tM=D\n"
+						"\t@%hu\n"
+						"\tD=A\n"
+						"\t@%s\n"
+						"\tM=M-D\n",
+						idx, symbol, pop_D, symbol, idx, symbol);
+				n_lines += 11;
 				break;
 			case TEMP:
 			case POINTER:
-				len = fprintf(asm_file, "%s\n@%hu\nM=D\n", pop_D, seg_base);
+				fprintf(asm_file,
+						"\t%s\n"
+						"\t@%hu\n"
+						"\tM=D\n", pop_D, seg_base + idx);
+				n_lines += 2;
 				break;
 			default:
 				break;
 		}
 	}
 	
-	return len;
+	return n_lines;
 }
 
 static size_t gen_push_lcl_asm(FILE *asm_file, uint16_t idx)
--- projects/07/vm_translator/vm_translator.h
+++ projects/07/vm_translator/vm_translator.h
@@ -1,10 +1,11 @@
 #ifndef __VM_TRANSLATOR_H__
 #define __VM_TRANSLATOR_H__
+
+
 #include <stdio.h>
 
-#define RAM_SIZE 32768		//32K	
 void* init_vm_translator(char *static_prefix);
-void destroy_vm_translator(void* vm_translator);
-int translate_vm_file(FILE* vm_file, FILE* asm_file);
+void destroy_vm_translator(void* handle);
+int translate_vm_file(const void* handle, FILE* vm_file, FILE* asm_file);
 
 #endif
