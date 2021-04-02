d83df62f4c4f667df15a9282ec5875ebd56ad467
haileyesus <hailejesus@gmail.com>
2021-04-02 18:02:23 +0200

    expand letStatement test cases

--- projects/software/cpp/compiler/tests/src/test_code_generator.cpp
+++ projects/software/cpp/compiler/tests/src/test_code_generator.cpp
@@ -62,9 +62,35 @@ bool testCmd(const string& cmd, const string& output, const vector<GeneratorType
     return cmpFiles(expectedFile, vmFile);
 }
 
-TEST_CASE("Integer Constant Term Generation", "[genTerm]") {
-    string cmd {"var int x;\nlet x = 5;"};
-	vector<GeneratorType> instructions { GeneratorType::VAR_DEC, GeneratorType::LET };
-	string output {"push constant 5\npop local 0"};
+TEST_CASE("Assign a constant to a local variable", "[genLetStatement]") {
+    // tests also varDec
+    string cmd {"var int x, int y; var int z; let z = 5;"};
+	vector<GeneratorType> instructions {
+        GeneratorType::VAR_DEC, GeneratorType::VAR_DEC, GeneratorType::LET
+    };
+	string output {"push constant 5\npop local 2"};
+	REQUIRE(testCmd(cmd, output, instructions)); 
+
+}
+
+TEST_CASE("Assign an expression to an argument", "[genLetStatement]") {
+    // tests also varDec and parameterList
+    string cmd {"(int x, int y) var int z; let y = -(x + z);"};
+	vector<GeneratorType> instructions {
+        GeneratorType::PARAM_LIST, GeneratorType::VAR_DEC, GeneratorType::LET
+    };
+	string output {"push argument 0\npush local 0\nadd\nneg\npop argument 1"};
+	REQUIRE(testCmd(cmd, output, instructions)); 
+}
+
+TEST_CASE("Array assignment") {
+    string cmd {"(int x, int y) var int a, int i; let a[i + 5] = ~(x + y);"};
+	vector<GeneratorType> instructions {
+        GeneratorType::PARAM_LIST, GeneratorType::VAR_DEC, GeneratorType::LET
+    };
+	string output {"push local 1\npush constant 5\nadd\n"
+                   "push local 0\nadd\npop pointer 1\n"
+                   "push argument 0\npush argument 1\nadd\nnot\n"
+                   "pop that 0"};
 	REQUIRE(testCmd(cmd, output, instructions)); 
 }
