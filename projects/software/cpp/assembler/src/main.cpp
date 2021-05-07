cad8d5b477a4257f4cf319c5e2bacc4cecac48fd
HaileyesusAK <h.kifle@tattile.com>
2021-05-07 11:31:38 +0200

    assembler: finish main program implementation

--- projects/software/cpp/assembler/src/main.cpp
+++ projects/software/cpp/assembler/src/main.cpp
@@ -1,8 +1,16 @@
-#include <iostream>
+#include <stdexcept>
+#include "assembler.hpp"
+
+using namespace std;
 
 int main(int argc, char* argv[]) {
-	(void)argc;
-	(void)argv;
+	if(argc != 2) {
+		throw length_error(string("Usage: ") + argv[0] + string(" <path>"));
+		return -1;
+	}
+
+	auto assembler = Assembler(argv[1]);
+	assembler.generate();
 
 	return 0;
 }
